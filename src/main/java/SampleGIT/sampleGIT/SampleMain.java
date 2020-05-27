36f8023b653aa3d88b953b5c132770f79946d2a2
Abhinav Kumar <abhinavmalviya1992@gmail.com>
2020-05-28 00:34:15 +0200

    Second commit

--- src/main/java/SampleGIT/sampleGIT/SampleMain.java
+++ src/main/java/SampleGIT/sampleGIT/SampleMain.java
@@ -7,5 +7,6 @@ public class SampleMain
     {
         System.out.println( "Hello World!" );
         System.out.println("Message Added");
+        System.out.println("Second Message Added");
     }
 }
